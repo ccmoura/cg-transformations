56a5a08af6162a5350f3ace0cda3efeb3f4b9ad8
Christopher Moura <christophermoura48@gmail.com>
2021-05-03 22:53:46 -0300

    animation

--- src/js/script.js
+++ src/js/script.js
@@ -115,10 +115,71 @@ var obj = { 'Add shape': () => {
       u_colorMult: [Math.random(), Math.random(), Math.random(), 1],
       u_matrix: m4.identity(),
     },
-    transformations.length-1
+    transformations.length - 1
   )
   }
 };
 
-gui.add(obj, 'Add shape');
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
+function shuffleArray(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+async function generateSmoothedAnimation(actual, target, index, t){
+  let x, y;
+  if(actual < target) {
+    x = Math.floor(actual);
+    y = Math.floor(target);
+  } else {
+    x = Math.floor(target);
+    y = Math.floor(actual);
+  }
+
+  while(x != y){
+    await sleep(10)
+    Object.assign(transformations[index], { 
+      [t]: {
+        [Object.keys(transformations[index][t])[0]]: x }
+      }
+    )
+    x += 0.01
+  }
+}
+
+var animateOne = { 'Animate One': async () => {
+    if(transformations.length === 0) 
+      return;
+    
+    const maxValue = 15;
+    const index = Math.floor(Math.random() * transformations.length);
+
+    for (const t of Object.keys(shuffleArray(transformations)[index])) {
+      await sleep(300)
+      const value = transformations[index][t][`${Object.keys(transformations[index][t])[0]}`];
+      generateSmoothedAnimation(value, Math.floor(Math.random() * (value + maxValue)) * (Math.round(Math.random()) == 0 ? 1 : -1), index, t);
+    }
+
+    
+  }
+}
+
+gui.add(obj, 'Add shape');
+gui.add(animateOne, 'Animate One');
