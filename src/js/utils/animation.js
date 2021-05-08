a2e67401d6a77d9eb3e7d4f6f1592eefc9565065
Christopher Moura <christophermoura48@gmail.com>
2021-05-08 02:01:50 -0300

    create animation class

--- src/js/utils/animation.js
+++ src/js/utils/animation.js
@@ -1,43 +1,45 @@
-function sleep(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
-function shuffleArray(array) {
-  let currentIndex = array.length;
-  let temporaryValue;
-  let randomIndex;
-
-  while (currentIndex !== 0) {
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
+class Animation {
+  static async sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
   }
 
-  return array;
-}
+  static shuffleArray(array) {
+    let currentIndex = array.length;
+    let temporaryValue;
+    let randomIndex;
 
-async function generateSmoothedAnimation(actual, target, index, t) {
-  let x;
-  let y;
-  if (actual < target) {
-    x = Math.floor(actual);
-    y = Math.floor(target);
-  } else {
-    x = Math.floor(target);
-    y = Math.floor(actual);
+    while (currentIndex !== 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    return array;
   }
 
-  while (x !== y) {
-    await sleep(10);
-    Object.assign(transformations[index], {
-      [t]: {
-        [Object.keys(transformations[index][t])[0]]: x,
-      },
-    });
-    x += 0.01;
+  static async generateSmoothedAnimation(actual, target, index, t) {
+    let x;
+    let y;
+    if (actual < target) {
+      x = Math.floor(actual);
+      y = Math.floor(target);
+    } else {
+      x = Math.floor(target);
+      y = Math.floor(actual);
+    }
+
+    while (x !== y) {
+      await this.sleep(10);
+      Object.assign(transformations[index], {
+        [t]: {
+          [Object.keys(transformations[index][t])[0]]: x,
+        },
+      });
+      x += 0.01;
+    }
   }
 }
 
@@ -48,13 +50,15 @@ const animateOne = {
     const maxValue = 15;
     const index = Math.floor(Math.random() * transformations.length);
 
-    for (const t of Object.keys(shuffleArray(transformations)[index])) {
-      await sleep(300);
+    for (const t of Object.keys(
+      Animation.shuffleArray(transformations)[index]
+    )) {
+      await Animation.sleep(300);
       const value =
         transformations[index][t][
           `${Object.keys(transformations[index][t])[0]}`
         ];
-      generateSmoothedAnimation(
+      Animation.generateSmoothedAnimation(
         value,
         Math.floor(Math.random() * (value + maxValue)) *
           (Math.round(Math.random()) === 0 ? 1 : -1),
